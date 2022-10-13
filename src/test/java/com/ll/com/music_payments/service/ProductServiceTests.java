1c1c35c60b3803521afcbbc424ed9adce826e709
chae-hwa <ihj0519@naver.com>
2022-10-13 11:45:43 +0900

    test : 상품 등록 및 수정 테스트 추가

---
+++ src/test/java/com/ll/com/music_payments/service/ProductServiceTests.java
@@ -0,0 +1,48 @@
+package com.ll.com.music_payments.service;
+
+import com.ll.com.music_payments.app.song.entity.Song;
+import com.ll.com.music_payments.app.song.service.SongService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+@ActiveProfiles("test")
+public class ProductServiceTests {
+
+    @Autowired
+    private SongService songService;
+
+    @Autowired
+    private ProductService productService;
+
+    @Test
+    @DisplayName("상품 등록")
+    void t1() {
+        Song song = songService.findById(1).get();
+
+        Product product = productService.create(song, "그리뭄", 1_900);
+
+        assertThat(product).isNotNull();
+        assertThat(product.getSubject()).isEqualTo("그리움");
+        assertThat(product.getPrice()).isEqualTo(1_900);
+    }
+
+    @Test
+    @DisplayName("상품 수정")
+    void t2() {
+        Product product1 = songService.findById(1).get();
+
+        productService.modify(product, "깊은 그리움", 3_200);
+
+        assertThat(product1).isNotNull();
+        assertThat(product1.getSubject()).isEqualTo("깉은 그리움");
+        assertThat(product1.getPrice()).isEqualTo(3_200);
+    }
+}
