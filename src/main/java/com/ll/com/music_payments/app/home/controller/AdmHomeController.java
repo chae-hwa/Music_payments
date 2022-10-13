9e7f76e871796560582765bde2f0ef9c3b5d1be4
chae-hwa <ihj0519@naver.com>
2022-10-14 01:14:56 +0900

    feat : ADMIN(관리자) 권한 회원만 관리자 페이지 접근 가능하도록

--- src/main/java/com/ll/com/music_payments/app/home/controller/AdmHomeController.java
+++ src/main/java/com/ll/com/music_payments/app/home/controller/AdmHomeController.java
@@ -1,5 +1,6 @@
 package com.ll.com.music_payments.app.home.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,12 +9,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/adm")
 public class AdmHomeController {
 
+
+    @PreAuthorize("hasAuthority('ADMIN')") // ADMIN 권한만 접근 가능
     @GetMapping("")
     public String showIdex() {
 
         return "redirect:/adm/home/main";
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping("home/main")
     public String showMain() {
 
